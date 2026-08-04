dbfa7bf59ac02be7dc84f75b5214b0e0c82cb9b9
Hridaya Saravanan <sarahridaya@gmail.com>
2026-08-04 13:34:46 -0700

    update CORS with production Vercel URL

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ async def websocket_telemetry(
 # CORS configuration to allow requests from the frontend application
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://your-app.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://vespr-nine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
